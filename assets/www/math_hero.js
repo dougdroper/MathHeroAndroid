0350e82e7b25f57324f3d1cc40d92d091db9028c
Douglas Roper <douglasroper@notonthehighstreet.com>
2012-04-16 22:37:35 +0100

    Align and generate sums

--- assets/www/math_hero.js
+++ assets/www/math_hero.js
@@ -2,6 +2,28 @@ $(document).ready(function(){
   var timer;
   var timer_is_on = true;
 
+  var getRandomNumber = function(){
+    return Math.round(Math.random(10) * 10);
+  };
+
+  var getRandomOperator = function(){
+    var val = getRandomNumber();
+    switch(true){
+      case (val < 4):
+        return "+";
+        break;
+      case (val >= 4 && val < 6):
+        return "*";
+        break;
+      case (val >= 6 && val < 8):
+        return "/";
+        break;
+      default:
+        return "-";
+        break;
+    }
+  };
+
   var movedown = function(){
     $("#calc_box").css('top', (parseInt($("#calc_box").css('top'), 10) + 5));
   };
@@ -13,6 +35,12 @@ $(document).ready(function(){
     }
   };
 
+  var populate_sum = function(){
+    $("#right_term").html(getRandomNumber());
+    $("#operation").html(getRandomOperator());
+    $("#left_term").html(getRandomNumber());
+  };
+
   var startGame = function(){
     timer_is_on = true;
     $("#pause_button").find(".ui-btn-text").html("Pause");
@@ -20,6 +48,9 @@ $(document).ready(function(){
   }
 
   $("#start_button").click(function(){
+    populate_sum();
+    clearTimeout(timer);
+    $("#calc_box").css('top', 0);
     startGame();
   });
 
